2de97062d36622a32656637b7895747a4a51a71f
Zerglrisk <wmf1235@naver.com>
2016-05-19 23:58:21 +0900

    It Finished Worked.
    it have a little error. but i dont realize that exception. but, it can
    be useful.

---
+++ monoalphabetic_cipher_cryptanalysis/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace monoalphabetic_cipher_cryptanalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtn_Plain = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Cipher = new System.Windows.Forms.RadioButton();
+            this.btn_fSave = new System.Windows.Forms.Button();
+            this.text_fPath = new System.Windows.Forms.TextBox();
+            this.Btn_fOpen = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.text_Cipher = new System.Windows.Forms.TextBox();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_Plain = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.text_CipherKey2 = new System.Windows.Forms.TextBox();
+            this.text_CipherKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtn_Plain);
+            this.groupBox1.Controls.Add(this.radioBtn_Cipher);
+            this.groupBox1.Controls.Add(this.btn_fSave);
+            this.groupBox1.Controls.Add(this.text_fPath);
+            this.groupBox1.Controls.Add(this.Btn_fOpen);
+            this.groupBox1.Location = new System.Drawing.Point(378, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(296, 80);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioBtn_Plain
+            // 
+            this.radioBtn_Plain.AutoSize = true;
+            this.radioBtn_Plain.Location = new System.Drawing.Point(77, 46);
+            this.radioBtn_Plain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioBtn_Plain.Name = "radioBtn_Plain";
+            this.radioBtn_Plain.Size = new System.Drawing.Size(47, 16);
+            this.radioBtn_Plain.TabIndex = 13;
+            this.radioBtn_Plain.TabStop = true;
+            this.radioBtn_Plain.Text = "평문";
+            this.radioBtn_Plain.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_Cipher
+            // 
+            this.radioBtn_Cipher.AutoSize = true;
+            this.radioBtn_Cipher.Location = new System.Drawing.Point(5, 46);
+            this.radioBtn_Cipher.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioBtn_Cipher.Name = "radioBtn_Cipher";
+            this.radioBtn_Cipher.Size = new System.Drawing.Size(59, 16);
+            this.radioBtn_Cipher.TabIndex = 12;
+            this.radioBtn_Cipher.TabStop = true;
+            this.radioBtn_Cipher.Text = "암호문";
+            this.radioBtn_Cipher.UseVisualStyleBackColor = true;
+            // 
+            // btn_fSave
+            // 
+            this.btn_fSave.Location = new System.Drawing.Point(220, 44);
+            this.btn_fSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_fSave.Name = "btn_fSave";
+            this.btn_fSave.Size = new System.Drawing.Size(70, 20);
+            this.btn_fSave.TabIndex = 11;
+            this.btn_fSave.Text = "저장";
+            this.btn_fSave.UseVisualStyleBackColor = true;
+            this.btn_fSave.Click += new System.EventHandler(this.btn_fSave_Click);
+            // 
+            // text_fPath
+            // 
+            this.text_fPath.Location = new System.Drawing.Point(5, 19);
+            this.text_fPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.text_fPath.Name = "text_fPath";
+            this.text_fPath.Size = new System.Drawing.Size(286, 21);
+            this.text_fPath.TabIndex = 10;
+            // 
+            // Btn_fOpen
+            // 
+            this.Btn_fOpen.Location = new System.Drawing.Point(146, 44);
+            this.Btn_fOpen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_fOpen.Name = "Btn_fOpen";
+            this.Btn_fOpen.Size = new System.Drawing.Size(70, 20);
+            this.Btn_fOpen.TabIndex = 9;
+            this.Btn_fOpen.Text = "열기";
+            this.Btn_fOpen.UseVisualStyleBackColor = true;
+            this.Btn_fOpen.Click += new System.EventHandler(this.Btn_fOpen_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_Encrypt);
+            this.groupBox2.Controls.Add(this.text_Cipher);
+            this.groupBox2.Controls.Add(this.btn_Decrypt);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.text_Plain);
+            this.groupBox2.Location = new System.Drawing.Point(10, 89);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(663, 344);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.Location = new System.Drawing.Point(297, 186);
+            this.btn_Encrypt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(70, 20);
+            this.btn_Encrypt.TabIndex = 15;
+            this.btn_Encrypt.Text = "<<암호화";
+            this.btn_Encrypt.UseVisualStyleBackColor = true;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // text_Cipher
+            // 
+            this.text_Cipher.BackColor = System.Drawing.SystemColors.Window;
+            this.text_Cipher.Location = new System.Drawing.Point(1, 23);
+            this.text_Cipher.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.text_Cipher.Multiline = true;
+            this.text_Cipher.Name = "text_Cipher";
+            this.text_Cipher.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_Cipher.Size = new System.Drawing.Size(295, 321);
+            this.text_Cipher.TabIndex = 11;
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Location = new System.Drawing.Point(297, 142);
+            this.btn_Decrypt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(70, 20);
+            this.btn_Decrypt.TabIndex = 14;
+            this.btn_Decrypt.Text = "복호화>>";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            this.btn_Decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "암호문";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "평문";
+            // 
+            // text_Plain
+            // 
+            this.text_Plain.Location = new System.Drawing.Point(368, 23);
+            this.text_Plain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.text_Plain.Multiline = true;
+            this.text_Plain.Name = "text_Plain";
+            this.text_Plain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_Plain.Size = new System.Drawing.Size(295, 321);
+            this.text_Plain.TabIndex = 12;
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Text files | *.txt";
+            // 
+            // sfd
+            // 
+            this.sfd.Filter = "Text files | *.txt";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.text_CipherKey2);
+            this.groupBox3.Controls.Add(this.text_CipherKey);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(10, 10);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(362, 80);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // text_CipherKey2
+            // 
+            this.text_CipherKey2.Location = new System.Drawing.Point(273, 18);
+            this.text_CipherKey2.Name = "text_CipherKey2";
+            this.text_CipherKey2.Size = new System.Drawing.Size(50, 21);
+            this.text_CipherKey2.TabIndex = 6;
+            this.text_CipherKey2.TextChanged += new System.EventHandler(this.text_CipherKey2_TextChanged);
+            // 
+            // text_CipherKey
+            // 
+            this.text_CipherKey.Location = new System.Drawing.Point(197, 18);
+            this.text_CipherKey.Name = "text_CipherKey";
+            this.text_CipherKey.Size = new System.Drawing.Size(50, 21);
+            this.text_CipherKey.TabIndex = 5;
+            this.text_CipherKey.TextChanged += new System.EventHandler(this.text_CipherKey_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "~";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "KEY";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(6, 18);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 442);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtn_Plain;
+        private System.Windows.Forms.RadioButton radioBtn_Cipher;
+        private System.Windows.Forms.Button btn_fSave;
+        private System.Windows.Forms.TextBox text_fPath;
+        private System.Windows.Forms.Button Btn_fOpen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.TextBox text_Cipher;
+        private System.Windows.Forms.Button btn_Decrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_Plain;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_CipherKey;
+        private System.Windows.Forms.TextBox text_CipherKey2;
+    }
+}
+
---
+++ monoalphabetic_cipher_cryptanalysis/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monoalphabetic_cipher_cryptanalysis
+{
+
+    public partial class Form1 : Form
+    {
+        int minLimitNumber = 0;
+        int maxLimitNumber = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Custom Init
+            radioBtn_Cipher.Select();
+            comboBox1.Items.Add("Additive Cipher");
+            comboBox1.Items.Add("Multiplicative Cipher");
+            comboBox1.SelectedIndex = 0;
+            minLimitNumber = 0;
+            maxLimitNumber = 25;
+        }
+
+        private void Btn_fOpen_Click(object sender, EventArgs e)
+        {
+            //reference : http://www.howtosolutions.net/2012/05/winform-open-file-browser-dialog-examples/
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                text_fPath.Text = ofd.FileName;
+                if (radioBtn_Cipher.Checked)
+                {
+                    text_Cipher.Text = File.ReadAllText(ofd.FileName);
+                }
+                else if (radioBtn_Plain.Checked)
+                {
+                    text_Plain.Text = File.ReadAllText(ofd.FileName);
+                }
+            }
+        }
+
+        private void btn_fSave_Click(object sender, EventArgs e)
+        {
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                text_fPath.Text = sfd.FileName;
+                if (radioBtn_Cipher.Checked)
+                {
+                    File.WriteAllText(sfd.FileName, text_Cipher.Text);
+                }
+                else if (radioBtn_Plain.Checked)
+                {
+                    File.WriteAllText(sfd.FileName, text_Plain.Text);
+                }
+            }
+
+        }
+        private void text_CipherKey_TextChanged(object sender, EventArgs e)
+        {
+            int val = 0;
+            bool res = Int32.TryParse(((TextBox)sender).Text, out val);
+
+            if (res && val < minLimitNumber)
+                text_CipherKey.Text = minLimitNumber.ToString();
+            else if (res && val > maxLimitNumber)
+                text_CipherKey.Text = maxLimitNumber.ToString();
+
+            text_CipherKey2.Text = text_CipherKey.Text;
+        }
+
+        private void text_CipherKey2_TextChanged(object sender, EventArgs e)
+        {
+            int val = 0;
+            bool res = Int32.TryParse(((TextBox)sender).Text, out val);
+            if (res && val < minLimitNumber)
+                text_CipherKey2.Text = minLimitNumber.ToString();
+            else if (res && val > maxLimitNumber)
+                text_CipherKey2.Text = maxLimitNumber.ToString();
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            string decrypt = "";
+            char[] array;
+
+            int val = 0;
+            bool res = Int32.TryParse(text_CipherKey.Text, out val);
+            int val2 = 0;
+            bool res2 = Int32.TryParse(text_CipherKey2.Text, out val2);
+
+            if (res && res2 && val > val2)
+            {
+                string temp;
+                temp = text_CipherKey.Text;
+                text_CipherKey.Text = text_CipherKey2.Text;
+                text_CipherKey2.Text = temp;
+            }
+
+            if (text_Cipher.Text == "")
+            {
+                MessageBox.Show("복호화할 문자열이 없습니다.");
+                return;
+            }
+                
+            else
+                array = text_Cipher.Text.ToLower().ToCharArray();
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: //Additive Cipher
+                    for (int i = val; i <= val2; i++)
+                    {
+                        decrypt += "Key : " + i.ToString() + "'s Decrypt\r\n";
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            int num = (int)array[j];
+                            if (num == 'a')
+                                num = 'h';
+                            else if (num == 'A')
+                                num = 'H';
+                            else if (num == 'i')
+                                num = 'i';
+                            else if (num == 'I')
+                                num = 'I';
+                            else if (num == 'r')
+                                num = 'r';
+                            else if (num == 'R')
+                                num = 'R';
+                            else if (num == 'j')
+                                num = 'q';
+                            else if (num == 'J')
+                                num = 'Q';
+                            else if (num >= 'a' && num <= 'z')
+                            {
+                                num -= i;
+                                if (num > 'z')
+                                    num = num - 26;
+
+                                if (num < 'a')
+                                    num = num + 26;
+
+                            }
+                            else if (num >= 'A' && num <= 'Z')
+                            {
+                                num -= i;
+                                if (num > 'Z')
+                                    num = num - 26;
+
+                                if (num < 'A')
+                                    num = num + 26;
+
+                            }
+                            decrypt += ((char)num).ToString();
+                        }
+                        decrypt += "\r\n\r\n";
+                    }
+                    text_Plain.Text = decrypt.ToLower();
+                    break;
+                case 1: //Multiplicative Cipher
+                    for (int i = val; i <= val2; i++)
+                    {
+                        decrypt += "Key : " + i.ToString() + "'s Decrypt\r\n";
+                        int key;
+                        if((key = FindGCDRelativePrime(26, i)) == -1)
+                        {
+                            decrypt += "Key의 역원이 없음.\r\n\r\n";
+                            continue;
+                        }
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            int num = (int)array[j];
+                            
+                            if (num >= 'a' && num <= 'z')
+                            {
+                                num -= 'a';
+                                num *= key;
+                                num %= 26;
+                                num += 'a';
+                            }
+                            else if (num >= 'A' && num <= 'Z')
+                            {
+                                num -= 'A';
+                                num *= key;
+                                num %= 26;
+                                num += 'A';
+                            }
+                            
+                            decrypt += ((char)num).ToString();
+                        }
+                        decrypt += "\r\n\r\n";
+                    }
+                    text_Plain.Text = decrypt.ToLower();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+            string encrypt = "";
+            char[] array;
+            int val;
+            bool res = Int32.TryParse(text_CipherKey.Text, out val);
+            int val2;
+            bool res2 = Int32.TryParse(text_CipherKey2.Text, out val2);
+
+
+            if (res && res2 && val > val2)
+            {
+                string temp;
+                temp = text_CipherKey.Text;
+                text_CipherKey.Text = text_CipherKey2.Text;
+                text_CipherKey2.Text = temp;
+            }
+            text_Cipher.Text = null;
+
+            if (text_Plain.Text == "")
+            {
+                MessageBox.Show("암호화할 문자열이 없습니다.");
+                return;
+            }
+                
+            else
+                array = text_Plain.Text.ToLower().ToCharArray();
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: //Additive Cipher
+
+                    for (int i = val; i <= val2; i++)
+                    {
+                        encrypt += "Key : " + i.ToString() + "'s Encrypt\r\n";
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            int num = (int)array[j];
+                            if (num == 'h')
+                                num = 'a';
+                            else if (num == 'H')
+                                num = 'a';
+                            else if (num == 'i')
+                                num = 'i';
+                            else if (num == 'I')
+                                num = 'I';
+                            else if (num == 'r')
+                                num = 'r';
+                            else if (num == 'R')
+                                num = 'R';
+                            else if (num == 'q')
+                                num = 'j';
+                            else if (num == 'Q')
+                                num = 'J';
+                            else if (num >= 'a' && num <= 'z')
+                            {
+                                num += i;
+                                if (num > 'z')
+                                    num = num - 26;
+
+                                if (num < 'a')
+                                    num = num + 26;
+
+                            }
+                            else if (num >= 'A' && num <= 'Z')
+                            {
+                                num += i;
+                                if (num > 'Z')
+                                    num = num - 26;
+
+                                if (num < 'A')
+                                    num = num + 26;
+
+                            }
+                            encrypt += ((char)num).ToString();
+                        }
+                        encrypt += "\r\n\r\n";
+
+                    }
+                    text_Cipher.Text = encrypt.ToUpper();
+                    break;
+                case 1: //Multiplicative Cipher
+                    for (int i = val; i <= val2; i++)
+                    {
+                        encrypt += "Key : " + i.ToString() + "'s Encrypt\r\n";
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            int num = (int)array[j];
+                            if (num >= 'a' && num <= 'z')
+                            {
+                                num -= 'a';
+                                num *= i;
+                                num %= 26;
+                                num += 'a';
+                            }
+                            else if (num >= 'A' && num <= 'Z')
+                            {
+                                num -= 'A';
+                                num *= i;
+                                num %= 26;
+                                num += 'A';
+                            }
+                            encrypt += ((char)num).ToString();
+                        }
+                        encrypt += "\r\n\r\n";
+                    }
+                    text_Cipher.Text = encrypt.ToLower();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private int FindGCDRelativePrime(int r1, int r2)
+        {
+            r1 = Math.Abs(r1);
+            r2 = Math.Abs(r2);
+            int t1 = 0, t2= 1 ;
+
+            // Pull out remainders.
+            for (;;)
+            {
+                int remainder = r1 % r2;
+                int t = t1 - (t2 * (r1 / r2));
+                if (remainder == 0)
+                {
+                    if (r2 == 1)
+                        return t2 > 0 ? t2 : t2+26;
+                    else
+                        return -1;
+                }
+                    
+                t1 = t2;
+                t2 = t;
+                r1 = r2;
+                r2 = remainder;
+            };
+        }
+    }
+}
